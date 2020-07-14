09716dfa61751b226a003a00a07eb7db9b08073d
Денисов Кирилл Олегович <kirill.denisov@gost-group.com>
2020-07-14 16:21:07 +0300

    vue.config.js fix

--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,6 @@ module.exports = {
     chainWebpack: config => {
     },
     devServer: {
-        /*proxy: {
-            '/admin': {
-                target: 'https://dev.goodt.org',
-                //pathRewrite: { '^/extensions': '' }
-            }
-        },*/
         watchOptions: {
             poll: true
         } 
